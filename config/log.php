171883391241322b3ba4370fbd9cb977272120a9
Nicolas <nicojs2011@gmail.com>
2026-02-18 15:40:15 -0400

    se movio la constante de config/log.php a include/log_generator.php y se cambio un poco el manejo de los logs

--- config/log.php
+++
@@ -1,5 +0,0 @@
-<?php 
-//Ruta donde se guardara los logs (No hace falta agregar __DIR__)
-// La carpeta debe de existir o dara error
-const LOGPATH = '/../../logs/';
-?>
--- includes/log_generator.php
+++ includes/log_generator.php
@@ -1,11 +1,14 @@
 <?php 
 // $msg_log = "El usuario con ID 45 ha iniciado sesión.";
 
-require_once __dir__. '/../config/log.php';
+const LOGPATH = __DIR__.'/../logs/';
 
 function log_generator($log_msg,$filename = 'actividad') : void {
+    if (!is_dir(LOGPATH)) {
+        mkdir(LOGPATH, 0777, true); // Crea la carpeta si no existe
+    }
     // El parámetro '3' indica que queremos escribir en un archivo específico
-    error_log($log_msg . PHP_EOL, 3, __DIR__.LOGPATH.$filename.".log");
+    error_log($log_msg . PHP_EOL, 3, LOGPATH.$filename.".log");
     // PHP_EOL: Es una constante que añade el salto de línea correcto según el sistema operativo (Windows o Linux).
 }
 ?>
